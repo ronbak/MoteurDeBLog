6e4d7e03f42ec66ece6abd53990726fcade56cc9
jcDeclunder <jeanchris.declunder@gmail.com>
2016-02-19 20:42:41 +0100

    commit Final pour code source

---
+++ connexion.inc.php
@@ -0,0 +1,7 @@
+<?php
+//Connection a la base de donnée
+@mysql_connect('mysql.hostinger.fr', 'u340761421_blog', 'azerty123456');
+mysql_select_db('u340761421_blog');
+mysql_query("SET NAMES 'utf8'");
+
+?>
